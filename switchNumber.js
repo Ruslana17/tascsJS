6387c876e840833166e023d6684638deeef7f532
Rusłana Fankowska <55513221+Ruslana17@users.noreply.github.com>
2023-03-07 12:56:10 +0100

    Add files via upload
    An implementation of a function that takes a number as input and returns an explanation of that number.

---
+++ switchNumber.js
@@ -0,0 +1,13 @@
+ const getNumberExplanation=(number)=>{
+   switch(number){
+     case 666:
+       return 'devil number';
+       case 42:
+       return 'answer for everything';
+       case 7:
+       return 'prime number'
+       default:
+       return null;
+   }
+ }
+ getNumberExplanation(7);
