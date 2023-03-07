52f23a3d4a8bd3c0b0ab1644e4109d238c7cfecd
Rusłana Fankowska <55513221+Ruslana17@users.noreply.github.com>
2023-03-07 13:06:33 +0100

    elseIfConstruct
    a function that takes the family name as input and returns one of three values.
    Definition rules:
    Friends ('friend'): 'Karstark', 'Tally'
    Enemies ('enemy'): 'Lannister', 'Frey'
    Any other families are considered neutral

---
+++ elseIfConstruct.js
@@ -0,0 +1,13 @@
+const whoIsThisHouseToStarks = (houseName)=>{
+    let status;
+  
+    if (houseName === 'Karstark' || houseName === 'Tally') {
+      status = 'friend';
+    } else if (houseName === 'Lannister' || houseName === 'Frey') {
+      status = 'enemy';
+    } else {
+      status = 'neutral';
+    }
+  
+    return status;
+  };
