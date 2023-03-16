b15872f324a4590686cbfb068ae1181e41d50a21
Rusłana Fankowska <55513221+Ruslana17@users.noreply.github.com>
2023-03-16 12:37:25 +0100

    Add files via upload
    a function that counts the number of times a letter appears in a sentence, case insensitive

---
+++ caseInsensitiveForInputWord.js
@@ -0,0 +1,12 @@
+const countChars = (str, char) => {
+    let i = 0;
+    let count = 0;
+  
+    while (i < str.length) {
+      if (str[i].toLowerCase() === char.toLowerCase()) {
+        count = count + 1;
+      }
+      i = i + 1;
+    }
+    return count;
+  };
