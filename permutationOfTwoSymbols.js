12f0e703397b6d69bba6f93a0030969330b3266c
Rusłana Fankowska <55513221+Ruslana17@users.noreply.github.com>
2023-03-20 11:13:46 +0100

    Add files via upload
    permutation of two consecutive input characters

---
+++ permutationOfTwoSymbols.js
@@ -0,0 +1,9 @@
+const encrypt = (str) =>{ 
+    let result ='';
+    for (let i = 0; i < str.length; i += 2){
+        const nextSymbol = str[i + 1] || '';
+        result = `${result}${nextSymbol}${str[i]}`;
+      }
+    
+      return result;
+    };
